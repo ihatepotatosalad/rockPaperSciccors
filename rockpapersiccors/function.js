9b35cc1c1edb496df5bad1ec3829fce7a8613168
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-05-29 21:28:50 -0400

    changes

--- rockpapersiccors/function.js
+++ rockpapersiccors/function.js
@@ -1,11 +1,49 @@
-const userChoice = document.querySelectorAll('[data-selection]')
-
-userChoice.forEach(userChoices =>{
-userChoices.addEventListener('click', e =>{
-    const choiceName = userChoices.dataset.selection
-    makeSelection(choiceName)
-})
-})
-function makeSelection(selection){
-    console.log(selection)
+const getUserChoice = (userInput) => {
+    userInput = userInput;
+    if (
+        userInput === "rock" ||
+        userInput === "paper" ||
+        userInput === "scissors"
+    ) {
+        return userInput;
+    } else {
+        console.log("error");
+    }
+};
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 }
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return console.log("its a tie");
+    }
+    if (userChoice === "rock" && computerChoice === "paper") {
+        return console.log("computer won");
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+        return console.log("User Won");
+    } else if (userChoice === "paper" && computerChoice === "scissors") {
+        return console.log("computer Won");
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+        return console.log("User Won");
+    } else if (userChoice === "scissors" && computerChoice === "paper") {
+        return console.log("User Won");
+    } else if (userChoice === "scissors" && computerChoice === "rock") {
+        return console.log("computer won");
+    }
+}
+function playGame() {
+    let userChoice = getUserChoice('rock')
+    let computerChoice = getComputerChoice()
+    determineWinner(userChoice, computerChoice)
+    console.log(userChoice)
+    console.log(computerChoice)
+}
+playGame()
